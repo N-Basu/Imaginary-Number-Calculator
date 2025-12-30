60a56354e490cd6fd2ee78368c98a1b135859041
Nirvik Basuroy <nirvikbasuroy@gmail.com>
2025-12-29 21:16:38 -0800

    Add files via upload

---
+++ ImagNumExpoCalc.scm
@@ -0,0 +1,31 @@
+(define (mod4 pwr)
+  (modulo pwr 4))
+
+(display "Enter i to any integer power: ")
+(newline)
+
+
+(define exp (read))
+
+
+(if (number? exp)
+  (if (integer? exp)
+    #t
+    (begin
+      (display "Invalid: Must be a whole number.")
+      (newline)
+      (exit)))
+  (begin
+      (display "Invalid: Must be numeric.")
+      (newline)
+      (exit)))
+      
+(define rem (mod4 exp))
+          
+(if (= rem 3)
+  (display "-i")
+  (if (= rem 2)
+    (display "-1")
+    (if (= rem 1)
+      (display "i")
+      (display "1"))))
