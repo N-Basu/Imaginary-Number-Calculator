672ef70b17b1534a087e20d89c31b5d4bcbfaeb0
Nirvik Basuroy <nirvikbasuroy@gmail.com>
2025-12-29 21:53:06 -0800

    Add files via upload

---
+++ NegativeSqrt(i).scm
@@ -0,0 +1,23 @@
+(define (imagi-root x)
+  (display "Square root (imaginary): ")
+  (display (sqrt (abs x)))
+  (display "i"))
+
+(display "Enter a number: ")
+(newline)
+
+(let ((input (read)))
+  (if (number? input)
+    (if (>= input 0)
+      (begin
+        (display "Square root: ")
+        (display (sqrt input))
+        (newline))
+      (imagi-root input))
+      (begin
+        (display "Invalid input")
+        (newline))))
+
+(newline)
+
+    
